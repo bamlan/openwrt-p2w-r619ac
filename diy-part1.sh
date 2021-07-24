5958734e43db1d8598180b4a3e9b98cc95d18ef7
但愿人长久 <bamlan@users.noreply.github.com>
2021-07-24 09:03:45 +0800

    Update diy-part1.sh
    add luci-app-ttnode to feeds

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+echo 'src-git xepher https://github.com/jerrykuku/luci-app-ttnode.git'>>feeds.conf.default
